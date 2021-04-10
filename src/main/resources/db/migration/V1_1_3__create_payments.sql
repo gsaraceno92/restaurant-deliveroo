69dec18627336e9e259412f033186d91e689ca43
giuseppe <saraceno-giuseppe@hotmail.com>
2021-04-10 23:43:54 +0200

    delete migration

--- src/main/resources/db/migration/V1_1_3__create_payments.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users` (
-
-    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` varchar(100) NOT NULL,
-    `phone_number` varchar(50) NOT NULL,
-    `description` varchar(100),
-);
