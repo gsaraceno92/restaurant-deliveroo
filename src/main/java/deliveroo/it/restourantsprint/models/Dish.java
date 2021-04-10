85e6ea66911958306eccbc752cebd67270811330
giuseppe <saraceno-giuseppe@hotmail.com>
2021-04-10 23:23:07 +0200

    fix: edit dish entity

--- src/main/java/deliveroo/it/restourantsprint/models/Dish.java
+++ src/main/java/deliveroo/it/restourantsprint/models/Dish.java
@@ -1,25 +1,47 @@
 package deliveroo.it.restourantsprint.models;
 
+import java.math.BigDecimal;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.NumberFormat;
 
 @Entity // This tells Hibernate to make a table out of this class
+@Table(name = "dishes")
 public class Dish {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
+    @NotBlank
+    @Column(unique = true)
+    @Size(max = 50)
     private String name;
-    private float price;
 
-    public Integer getId() {
+    @NotBlank
+    @NumberFormat(pattern = "#,##,###,###.##,#.#,##.#,###.#")
+    private BigDecimal price;
+
+    @Size(max = 100)
+    private String category;
+
+    @Size(max = 200)
+    private String description;
+
+    private Boolean available;
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -27,15 +49,39 @@ public class Dish {
         return this.name;
     }
 
-    public float getPrice() {
-        return this.price;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setPrice(float price) {
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
         this.price = price;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDecription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getAvailable() {
+        return this.available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
     }
 }
